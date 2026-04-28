1c9b4df794df9a90fd6e756c83b826ee21116659
German World Club <germanworldclub@Mac-mini.local>
2026-04-28 13:48:50 +0400

    fix header undefined values when users are logged out

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -21,28 +21,28 @@ const Header: React.FC<Props> = () => {
 
 
     const navigate = useNavigate();
-        const {setToken} = useContext(AuthContext)
-        
-        const logout = () => {
-        
-            StorageService.removeToken();
-            StorageService.removeRoles();
-               setToken(null)
-               Cookies.remove('__i_a_u');
-               
-               navigate('/login')
-    
-        
-       
-        };
+    const { setToken } = useContext(AuthContext)
+
+    const logout = () => {
+
+        StorageService.removeToken();
+        StorageService.removeRoles();
+        setToken(null)
+        Cookies.remove('__i_a_u');
+
+        navigate('/login')
+
+
+
+    };
 
 
     const dateTime = CHookDateTime();
     const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-    const {token} = useContext(AuthContext)
-        
+    const { token } = useContext(AuthContext)
+
     useEffect(() => {
-       
+
     }, [])
 
     const user = localStorage.getItem('user');
@@ -57,59 +57,63 @@ const Header: React.FC<Props> = () => {
 
 
     return (
-        <div className='flex align-items-center p-2 justify-content-between header-navbar' style={{backgroundColor: '#2b2c38'}}>
+        <div className='flex align-items-center p-2 justify-content-between header-navbar' style={{ backgroundColor: '#2b2c38' }}>
 
             <div className='flex align-items-center '>
-            <Sidebar visible={isSidebarOpen}  className='p-0 m-0' onHide={()=>{
-                setIsSidebarOpen(false);
-            }}>
-                <Navbar close={closeSidebar}/>
-            </Sidebar>
+                <Sidebar visible={isSidebarOpen} className='p-0 m-0' onHide={() => {
+                    setIsSidebarOpen(false);
+                }}>
+                    <Navbar close={closeSidebar} />
+                </Sidebar>
 
 
-                <Button 
-                    className='text-white p-button-link none' 
-                    icon={'pi pi-bars'} 
-                    onClick={openSidebar} disabled={!token} 
-                    style={{ 
-                        opacity: token ? 1 : 0, 
+                <Button
+                    className='text-white p-button-link none'
+                    icon={'pi pi-bars'}
+                    onClick={openSidebar} disabled={!token}
+                    style={{
+                        opacity: token ? 1 : 0,
                         visibility: token ? "visible" : "hidden",
                         transition: "opacity 0.3s ease"
-                    }}/>
-                    <div className='flex align-items-center'>
-                        <img src={logo} height={50}/>
-                        <div className='flex flex-column pl-3'>
-                            <span className='text-white p-0 pb-1' ><strong>{config.APP_NAME}</strong></span>
-                            <span className='text-white p-0' style={{fontSize: 10}}>Admin Area</span>
-                        </div>
+                    }} />
+                <div className='flex align-items-center'>
+                    <img src={logo} height={50} />
+                    <div className='flex flex-column pl-3'>
+                        <span className='text-white p-0 pb-1' ><strong>{config.APP_NAME}</strong></span>
+                        <span className='text-white p-0' style={{ fontSize: 10 }}>Admin Area</span>
                     </div>
+                </div>
             </div>
-            <div>   
-                 <div className='flex align-items-center' style={{color: 'white', fontWeight: 300, fontSize: 15}}>
+            <div>
+                <div className='flex align-items-center' style={{ color: 'white', fontWeight: 300, fontSize: 15 }}>
 
-                        <strong className={`pr-3 flex`} style={{fontSize: '0.8rem'}}>
-                            <div className='pr-3'>
+                    <strong className={`pr-3 flex`} style={{ fontSize: '0.8rem' }}>
+                        <div className='pr-3'>
 
-                            <p> Logged in as <strong>{user}</strong></p> 
-                            </div>
-                            <div>
+                            {token && <p> Logged in as <strong>{user}</strong></p>}
+                        </div>
+                        <div>
 
                             <p>
+                                {token && <span className='mx-3'>|</span>}
+
+                                <ClockComponent />
                                 <span className='mx-3'>|</span>
-                                <ClockComponent/>
-                                <span className='mx-3'>|</span>
-                                </p> 
-                            </div>
-                        </strong>
-                        
-                        <Tooltip target=".logout-icon" style={{ fontSize: 10, whiteSpace:'nowrap'}}/>
-                            <div onClick={logout} 
-                                data-pr-tooltip="Logout" 
-                             data-pr-position="left"
+                            </p>
+                        </div>
+                    </strong>
+
+                    {token && <>
+                        <Tooltip target=".logout-icon" style={{ fontSize: 10, whiteSpace: 'nowrap' }} />
+                        <div onClick={logout}
+                            data-pr-tooltip="Logout"
+                            data-pr-position="left"
                             style={{ cursor: 'pointer', display: !token ? 'none' : 'block' }} className='flex logout-icon align-items-center'>
-                                <i className="pi pi-sign-out mr-2" style={{ fontSize: '1rem' }}></i><span style={{ fontSize: '0.8rem' }}>{`  Logout`}</span>
-                            </div>
-                    </div>
+                            <i className="pi pi-sign-out mr-2" style={{ fontSize: '1rem' }}></i><span style={{ fontSize: '0.8rem' }}>{`  Logout`}</span>
+                        </div>
+                    </>}
+
+                </div>
             </div>
         </div>
     )
